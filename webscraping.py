46b02399326ad1b75ff3df38233d348f491c973b
Subeesh V <44514519+Subeesh-V@users.noreply.github.com>
2021-09-17 16:11:09 +0530

    Add files via upload

---
+++ webscraping.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+print("Enter the type of image(Eg:Happy Images,Crying Images etc.")
+image = input('>')
+url = f"https://unsplash.com/s/photos/{image}"
+r = requests.get(url).text
+soup = BeautifulSoup(r,"html5lib")
+images = soup.find_all("img","oCCRx")
+for imag in images:
+    print(imag.get("src"))
+    print("")
+
+
