545b3bc2ff85899269214e32d34d58635e8cd595
Subeesh V <44514519+Subeesh-V@users.noreply.github.com>
2021-09-17 16:47:05 +0530

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -5,7 +5,7 @@ image = input('>')
 url = f"https://unsplash.com/s/photos/{image}"
 r = requests.get(url).text
 soup = BeautifulSoup(r,"html5lib")
-images = soup.find_all("img","oCCRx")
+images = soup.find_all("img",class_="oCCRx")
 for imag in images:
     print(imag.get("src"))
     print("")
